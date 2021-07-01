ddaed8fdca35d7ee616f94808a499125a5119532
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-07-01 11:09:41 -0700

    update daily horoscope

--- src/components/DailyHoroscope/index.js
+++ src/components/DailyHoroscope/index.js
@@ -1,15 +1,30 @@
-import React from "react"; 
+import React, { Component } from "react"; 
+
 // Aries,Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces
 //need to pass through button class & selection selectZodiac
-function DailyHoroscope() {
+function DailyHoroscope(props) {
+    const {
+        sign = [],
+        setCurrentSign,
+        currentSign,
+    } = props;
+
     const URL = 'https://aztro.sameerkumar.website/?sign=' + {sign} + '&day=today';
     fetch(URL, {
         method: 'POST'
     }).then(response => response.json())
     .then(function(data) {
+
+        //map over a link to value in array
         switch (data) {
         case "aries":
-
+            <div>
+            <h2>{currentSign.zodiac}</h2>
+            <span onClick={() => {
+                setCurrentSign(sign);
+            }}></span>
+            {/* <p>{json.description}</p> */}
+            </div>
         break;
         case "taurus":
 
--- src/components/pages/Horoscope.js
+++ src/components/pages/Horoscope.js
@@ -1,69 +1,94 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import DailyHoroscope from "../DailyHoroscope";
 // import ZodiacSearch from "../FindZodiac";
 import DailyHoroscope from "../DailyHoroscope";
 import "../../App.css";
+import { useParams } from "react-router-dom";
 // import HoroscopeApi from "aztro-js";
 // Aries,Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces
-
+//add value for each sign
 function Horoscope() {
   const [sign] = useState([
     {
         zodiac: "Aries",
-        icon: ""
+        icon: "",
+        value: 0
     },
     {
         zodiac: "Taurus",
-        icon: ""
+        icon: "",
+        value: 1
     },
     {
         zodiac: "Gemini",
-        icon: ""
+        icon: "",
+        value: 2
     },
     {
         zodiac: "Cancer",
-        icon: ""
+        icon: "",
+        value: 3
     },
     {
         zodiac: "Leo",
-        icon: ""
+        icon: "",
+        value: 4
     },
     {
         zodiac: "Virgo",
-        icon: ""
+        icon: "",
+        value: 5
     },
     {
         zodiac: "Libra",
-        icon: ""
+        icon: "",
+        value: 6
     },
     {
         zodiac: "Scorpio",
-        icon: ""
+        icon: "",
+        value: 7
     },
     {
         zodiac: "Sagittarius",
-        icon: ""
+        icon: "",
+        value: 8
     },
     {
         zodiac: "Capricorn",
-        icon: ""
+        icon: "",
+        value: 9
     },
     {
         zodiac: "Aquarius",
-        icon: ""
+        icon: "",
+        value: 10
     },
     {
         zodiac: "Pisces",
-        icon: ""
+        icon: "",
+        value: 11
     },
   ]);
+
+ const [currentSign, setCurrentSign] = useState(null);
+ const { value } = useParams();
+ const handleButton = useEffect(null);
+
+useEffect(
+    handleButton = (e) => {
+        setCurrentSign = e.target.value
+    },
+
+)
 
   return (
     <div>
       <h1 className="horoscope">HOROSCOPE</h1>
       {/* <ZodiacSearch /> */}
+      {/* e.target value and map */}
       {sign.map((sign) => (
-          <button onClick={DailyHoroscope} type="submit">{sign.zodiac}</button>
+          <button onClick={(e)=>(handleButton(e))} type="submit">{sign.zodiac}</button>
       ))}
     </div>
   );
