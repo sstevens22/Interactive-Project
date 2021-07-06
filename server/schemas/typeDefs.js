e2767fb974ae807685ed01a83374bbd503390d90
Shivvers <ashivvers95@gmail.com>
2021-07-05 18:42:21 -0700

    json web token functional

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,7 +15,7 @@ type Query {
 }
 
 type Mutation {
-    login(email: String!, password: String!): User
+    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
 }
 
