25a8ec066eea13bea46f09b4eafa1864e39efbc8
Sheldon Stevens <sgste080@yahoo.com>
2021-07-07 13:09:44 -0700

    logout done

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -32,24 +32,18 @@ function Footer() {
             <Link to='/sign-up'>How it works</Link>
             <Link to='/'>Testimonials</Link>
             <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
             <Link to='/'>Terms of Service</Link>
           </div>
           <div class='footer-link-items'>
             <h2>Contact Us</h2>
             <Link to='/'>Contact</Link>
             <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
+            <h2>Donate</h2>
+            <Link to='/'>Donations</Link>
           </div>
           <div class='footer-link-items'>
             <h2>Social Media</h2>
